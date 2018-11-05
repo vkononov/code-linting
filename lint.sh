c858e5374ef8e02c3fc2ac271cb36862caa61f1c
Vadim Kononov <vadim@poetic.com>
2018-11-05 11:07:35 -0600

    Fixed some minor issues with coffee linting.

--- lint.sh
+++ lint.sh
@@ -2,29 +2,31 @@
 
 LINTERS=('coffee' 'haml' 'ruby' 'rails' 'sass', 'scss')
 
-RAW_URL='https://raw.githubusercontent.com/vkononov/code-linting/master/linters'
+IGNORE_URL='https://raw.githubusercontent.com/vkononov/code-linting/master/ignore'
+LINTER_URL='https://raw.githubusercontent.com/vkononov/code-linting/master/linters'
 
 COFFEE_EXEC='coffeelint'
 COFFEE_CONF='coffeelint.json'
-COFFEE_LINK="$RAW_URL/$COFFEE_CONF"
+COFFEE_IGNR='.coffeelintignore'
+COFFEE_LINK="$LINTER_URL/$COFFEE_CONF"
 COFFEE_PATH=$(pwd)
-COFFEE_ARGS=''
+COFFEE_ARGS='.'
 
 HAML_EXEC='haml-lint'
 HAML_CONF='.haml-lint.yml'
-HAML_LINK="$RAW_URL/$HAML_CONF"
+HAML_LINK="$LINTER_URL/$HAML_CONF"
 HAML_PATH=$(pwd)
 HAML_ARGS=''
 
 RUBOCOP_EXEC='rubocop'
 RUBOCOP_CONF='.rubocop.yml'
-RUBOCOP_LINK="$RAW_URL/$RUBOCOP_CONF"
+RUBOCOP_LINK="$LINTER_URL/$RUBOCOP_CONF"
 RUBOCOP_PATH=$(pwd)
 RUBOCOP_ARGS='--auto-correct'
 
 SASS_EXEC='sass-lint'
 SASS_CONF='.sass-lint.yml'
-SASS_LINK="$RAW_URL/$SASS_CONF"
+SASS_LINK="$LINTER_URL/$SASS_CONF"
 SASS_PATH=$(pwd)
 SASS_ARGS='--no-exit --verbose'
 
@@ -54,6 +56,15 @@ lint_coffee_script() {
 		npm install ${COFFEE_EXEC}
 	fi
 
+    status "Looking for local $COFFEE_IGNR in $COFFEE_PATH"
+    if test -e "$COFFEE_PATH/$COFFEE_IGNR"; then
+		echo "Found $COFFEE_IGNR in $COFFEE_PATH"
+	else
+	    coffeelintignore_found=true
+		echo "Local $COFFEE_IGNR not found in $COFFEE_PATH, using default"
+		curl -o ${COFFEE_IGNR} "$IGNORE_URL/$COFFEE_IGNR"
+	fi
+
 	status "Looking for local $COFFEE_CONF in $COFFEE_PATH"
 	if test -e "$COFFEE_PATH/$COFFEE_CONF"; then
 		echo "Found $COFFEE_CONF in $COFFEE_PATH"
@@ -62,12 +73,16 @@ lint_coffee_script() {
 		echo "Local $COFFEE_CONF not found in $COFFEE_PATH, using default"
 		status "Linting CoffeeScript with $COFFEE_LINK"
 		curl -o "$TMP_PATH@$COFFEE_CONF" ${COFFEE_LINK}
-		COFFEE_ARGS="$COFFEE_ARGS --config $TMP_PATH@$COFFEE_CONF"
+		COFFEE_ARGS="$COFFEE_ARGS --file $TMP_PATH@$COFFEE_CONF"
 	fi
 
 	command="$NODE_BIN_PATH/$COFFEE_EXEC $COFFEE_ARGS"
 	warning "Running <$command>"
 	eval ${command} || { valid=false; }
+
+	if ${coffeelintignore_found} = true; then
+	    rm ${COFFEE_IGNR}
+	fi
 }
 
 lint_haml() {
